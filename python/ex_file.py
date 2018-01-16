061b223410698e19e281f5175d457f32caf8099c
Paramaguru <mrrparamaguru.git@gmail.com>
2018-01-16 15:16:17 +0530

    function to remove all punctutaions and spaces in a file

--- python/ex_file.py
+++ python/ex_file.py
@@ -20,7 +20,8 @@ def whitespace_remover(item,pos):
 	global white_list
 	global white_lenth
 	if(pos>(white_lenth-1)):
-		print(item)
+		if(item!=''):
+			print(item)
 		return
 	temp_list = item.strip().split(white_list[pos])
 	for items in temp_list:
@@ -32,31 +33,20 @@ def punc_remover(item,pos):
 		whitespace_remover(item,0)
 		return
 	temp_list = item.strip().split(li[pos])
-	#print(temp_list)	
 	for items in temp_list:
 		punc_remover(items,pos+1)
 
 
-st=" this is , a string }}}} and it is . needed !\"#$%&\'\()*+,-./:;<=>?@[\\]^_`{|}~ definitely"
-length=len(li)
-print(length)
-punc_remover(st,0)
+#st=" this is , a string }}}} and it is . needed !\"#$%&\'\()*+,-./:;<=>?@[\\]^_`{|}~ definitely"
+#length=len(li)
+#print(length)
+#punc_remover(st,0)
 #li = st.strip().split(li[0])
 
 
-#print(li)
+def task_1():
+	fd = open("tmp_read")
+	for line in fd:
+		punc_remover(line,0)
 
-
-
-#punc = string.punctuation
-#for let in punc:
-#	print(let)
-
-#def task_1():
-#	fd = open("tmp_read")
-#	for line in fd:
-#		for one_punc in punc:
-#			words=line.strip().split(one_punc)
-#		print(one_line)
-
-#task_1()
+task_1()
