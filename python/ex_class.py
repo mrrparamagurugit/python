ee4198b2e597cd12923f07071737cfd340eea9c4
Paramaguru <mrrparamaguru.git@gmail.com>
2018-01-18 19:46:43 +0530

    changes in class file

--- python/ex_class.py
+++ python/ex_class.py
@@ -10,18 +10,18 @@ class First:
 		self.mynewvar=1;
 
 	def __add__(self,one):
+		print("add called")
 		self.myvar=self.myvar+one
 		return self.myvar
 
 	def __str__(self):
-		print("my first class",self.myvar)
-		return self
+		return "my first class %d" % (self.myvar)
 #	def class_fun(self):
 #		print("%2d %2d" %(self.myvar,self.mynewvar))
 
 
 newobj = First()
-
+secondobj= First()
 #newobj.myvar=100
 #newobj.mynewvar=200
-print(newobj)
+print(newobj+ secondobj)
