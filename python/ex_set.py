3679e702eb32cde058549416a152b8454a868f9a
Paramaguru <mrrparamaguru.git@gmail.com>
2018-01-23 17:36:37 +0530

    example code for default dict and set

---
+++ python/ex_set.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+from collections import defaultdict
+
+val = ['1','2','3','4','11','1']
+
+print(set(val))
+
+print(list)
+
+#DEFAULT DICT
+
+
+
+dic= defaultdict(list)
+#dic= {}
+print("dic", dic)
+
+dic["one"]=1 ; dic["two"]=2 ; t= dic["onee"]
+y = dic["doo"]
+print(dic)
+
+print(dic["oneee"])
+print(type(dic["oneeee"]))
+#print(str(dic))
+
+#for key in dic:
+#	print(key,dic[key])
