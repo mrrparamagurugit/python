274f0f7de4fa39744a7b7c6d75abb9b458b4b001
Paramaguru <mrrparamaguru.git@gmail.com>
2018-01-23 19:30:38 +0530

    sample server socket

--- python/ex_class.py
+++ python/ex_class.py
@@ -1,27 +1,85 @@
 #!/usr/bin/python3
 
+bigglobal = []
+
 class First:
 
-#	myvar=0;
-#	mynewvar=1;
+	myvar=0;
+	mynewvar=1;
+	called_count=10
+	mylist = []
+	mydic= {}
 	def __init__(self):
 		print("init called");
 		self.myvar=0;
 		self.mynewvar=1;
+		global bigglobal
+		bigglobal.append(self)
+		self.mylist.append(self)
 
 	def __add__(self,one):
-		print("add called")
-		self.myvar=self.myvar+one
+		print(" frist add called")
+		self.myvar=self.myvar+one.tempu
 		return self.myvar
 
 	def __str__(self):
 		return "my first class %d" % (self.myvar)
+
+	def sample_fun(self):
+		print("class first")	
+		self.called_count+=1
+
+	def __radd__(self,newone):
+		print("first radd called")
+
+#	def sample_checker(self,nextclass):
+#		print self.sample_fun(self) + nextclass.sample_fun()
 #	def class_fun(self):
 #		print("%2d %2d" %(self.myvar,self.mynewvar))
 
 
-newobj = First()
-secondobj= First()
-#newobj.myvar=100
-#newobj.mynewvar=200
-print(newobj+ secondobj)
+class Second:
+
+
+	def __init__(self):
+		print("second init called")
+		global bigglobal
+		bigglobal.append(self)
+		
+	called_count=0
+	def sample_fun(self,fff):
+		print("class second")
+		#global called_count
+		self.called_count+=1
+		fff.sample_fun()
+
+	def __add__(self,one):
+                print(" second add called")
+
+	def __radd__(self,newonw):
+                print("second radd called")
+
+
+#	def sample_checker(self,nextclass):
+#		print sample_fun() + nextclass.sample_fun()
+
+
+
+
+#newobj = First()
+#secondobj= Second()
+#secondobj.sample_fun(newobj)#secondobj.sample_checker(newobj)
+#secondobj.sample_fun(newobj)
+#print(secondobj.called_count)
+#print(newobj.called_count)
+
+#print(newobj+secondobj)
+#print(secondobj+newobj)
+#print(1+newobj)
+
+#print(vars(newobj))
+
+#print(getattr(newobj,'mylist'))
+
+print(bigglobal)
+print(globals())
---
+++ python/ex_server_socket.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import socket
+
+socketid= socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+host = socket.gethostname()
+
+port = 12346
+
+socketid.bind((host,port))
+socketid.listen(3)
+
+while True:
+#	print("server running\n")
+	clientid,addr=socketid.accept()
+#	print("Got a connection from %s" % str(addr))
+	print("%s: " % clientid.recv(100).decode('ascii') )
+	msg = 'Thank you for connecting'+ "\r\n"
+	clientid.send(msg.encode('ascii'))
+	clientid.close()
+
+
