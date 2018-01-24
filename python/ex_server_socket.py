e6f9e0891107725f2f9390f25f85d6f16094ccbd
Paramaguru <mrrparamaguru.git@gmail.com>
2018-01-24 17:43:39 +0530

    sample server socket

--- python/ex_server_socket.py
+++ python/ex_server_socket.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 
 import socket
-
 socketid= socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
 host = socket.gethostname()
-
+ip = socket.gethostbyname(host)
+host = '10.9.247.226'
+print(ip)
+host = ip
 port = 12346
 
 socketid.bind((host,port))
