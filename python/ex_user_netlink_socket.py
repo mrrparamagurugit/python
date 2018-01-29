131bdf7b5f455b5a887c7f4fb21397a0c204577d
Paramaguru <mrrparamaguru.git@gmail.com>
2018-01-29 19:33:27 +0530

    netlink user mode socket updated

--- python/ex_user_netlink_socket.py
+++ python/ex_user_netlink_socket.py
@@ -3,6 +3,11 @@
 import socket as s
 
 fd = s.socket(s.AF_NETLINK,s.SOCK_DGRAM,23)
-
-print(dir(s))
+#msg = 'Thank you for connecting'+ "\r\n"
+chat=input(":")
+space=' ' * 16 
+new = space + chat #+ '\0'
+fd.send(new.encode('ascii'))
+ # clientid.send(msg.encode('ascii'))
+#print(dir(s))
 
